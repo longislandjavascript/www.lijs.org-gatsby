1c780c52549bc01e7de113610df622f54a261d61
gojutin <gojutin@gmail.com>
2020-05-01 15:36:10 -0400

    Updated skip link styles.

--- src/components/skip-link.tsx
+++ src/components/skip-link.tsx
@@ -3,13 +3,20 @@ import styled from "styled-components";
 export const SkipLink = styled.a`
   position: fixed;
   top: -40px;
-  left: 0;
-  background: ${p => p.theme.colors.logoBlue};
-  color: white;
+  left: 10px;
+  background: ${p => p.theme.colors.blueDarkest};
+  color: ${p => p.theme.colors.textLight};
   padding: 4px;
   z-index: 99999;
+  transition: top 200ms, left 200ms;
+  padding: 4px;
+  border-radius: 4px;
+  text-decoration: none;
+  outline: none;
+  border: 2px solid ${p => p.theme.colors.logoYellow};
 
   &:focus {
-    top: 0;
+    top: 10px;
+    left: 10px;
   }
 `;
