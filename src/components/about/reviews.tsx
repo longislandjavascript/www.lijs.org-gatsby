13171721af9323f7dcfecf0fc4a604b313b1beae
gojutin <gojutin@gmail.com>
2020-08-04 21:25:57 -0400

    Extracted reviews to `data` directory.

--- src/components/about/reviews.tsx
+++ src/components/about/reviews.tsx
@@ -1,22 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Card } from "../card";
-
-export const reviewsData = [
-  [
-    "Danika Wiegand",
-    "My first meeting was very informative - I look forward to the next one! Great people",
-  ],
-  ["Michael Boracci", "Great"],
-  ["Chris Early", "Excellent community!"],
-  ["Yujin Lin", "Very good. People are friendly and all have good spirit."],
-  ["Anthony Alvarez", "Great!"],
-  ["John Rattinger", "So far, so good... My first meeting but i will be back!"],
-  [
-    "Scott DeVito (future organizer)",
-    "Informative and welcoming. Will definitely be back next month.",
-  ],
-];
+import { reviews } from "../../data/reviews";
 
 export const Reviews = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -24,7 +9,7 @@ export const Reviews = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex(i => {
-        if (i >= reviewsData.length - 1) {
+        if (i >= reviews.length - 1) {
           return 0;
         } else {
           return i + 1;
@@ -34,7 +19,7 @@ export const Reviews = () => {
     return () => clearInterval(interval);
   }, []);
 
-  const [member, message] = reviewsData[currentIndex];
+  const [member, message] = reviews[currentIndex];
 
   return (
     <Card title="Reviews">
---
+++ src/data/reviews.ts
@@ -0,0 +1,15 @@
+export const reviews = [
+  [
+    "Danika Wiegand",
+    "My first meeting was very informative - I look forward to the next one! Great people",
+  ],
+  ["Michael Boracci", "Great"],
+  ["Chris Early", "Excellent community!"],
+  ["Yujin Lin", "Very good. People are friendly and all have good spirit."],
+  ["Anthony Alvarez", "Great!"],
+  ["John Rattinger", "So far, so good... My first meeting but i will be back!"],
+  [
+    "Scott DeVito (future organizer)",
+    "Informative and welcoming. Will definitely be back next month.",
+  ],
+];
